b0317edfee2921e40b804c99547a26b69e7aecd0
Katrine Ren <s3617315@student.rmit.edu.au>
2017-10-04 13:47:03 +1100

    Add image detail view from Rita building save button remove the legacy code and fix the merging markers

--- Circus-Imgs/Images Feed Table View Controller.swift
+++ Circus-Imgs/Images Feed Table View Controller.swift
@@ -7,27 +7,46 @@
 //
 
 import UIKit
-import SwiftyJSON
+//import SwiftyJSON
 
-    //move to Model
-    let apiKey = "c21fa3df81455afb0cd447e13ea34179"
+    
 
+let dataModelDidUpdateNotif = "dataModelDidUpdateNotif"
 
 class ImagesFeedTableViewController: UITableViewController {
     
-    @IBAction func favourite(_ sender: Any) {
-       // Model.get.saveMovie(	name.text!, subtitle: desc.text, imageName: imageName.text!, existing: currentMovie)
-        
-        self.navigationController?.popToRootViewController(animated: true)
+    //handle observer
+    let loadedData = Notification.Name(rawValue: dataModelDidUpdateNotif)
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         self.tableView.contentInset = UIEdgeInsetsMake(30,0,0,0)
-        downloadJSON()
+        Model.get.downloadJSON()
+        createObeservers()
     }
 
+    //observers
+    func createObeservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(getDataUpdate), name: NSNotification.Name(rawValue: dataModelDidUpdateNotif), object: nil)
+    }
+    
+    
+    //reload table after JSON is downloaded
+    func getDataUpdate(){
+        
+        OperationQueue.main.addOperation(
+            {
+                //update table here
+                self.tableView.reloadData()
+        })
+    }
+    
     
     override func didReceiveMemoryWarning()
     {
@@ -42,163 +61,63 @@ class ImagesFeedTableViewController: UITableViewController {
         return 1
     }
     
-        //returns a set of images each load
+    //returns a set of images each load
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
         
-        return photosArray.count
+        
+        //return photosArray.count
         //when JSON is in Model
-        //return Model.get.photosArray.count
-
+        return Model.get.photosArray.count
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ImageCell") as! MyCell
         
-        let photo = photosArray[indexPath.row]
+        //let photo = photosArray[indexPath.row]
         
         //when JSON is in Model
-        //let photo = Model.get.photosArray[indexPath.row]
+        let photo = Model.get.photosArray[indexPath.row]
         
         cell.labelRight.text = photo.photoTitle
         
-    
+        
         let imgLink = URL(string: photo.photoURL)
         
         if (imgLink != nil) {
             let data = NSData(contentsOf: (imgLink)!)
             cell.myImage.image = UIImage(data: data! as Data)
         }
- 
+        
         return cell
     }
     
-    
-    //For DetailView after Image is tapped
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    override func prepare (for segue: UIStoryboardSegue, sender: Any?)
     {
-
-    }
-    
-
-    
-    //REST API Call - move to Model
-    final let urlString = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=\(apiKey)&extras=geo%2C+tags%2C+machine_tags%2C+url_z&format=json&nojsoncallback=1"
-    
-    //create Photos array from Model
-    var photosArray = [Photos]()
-    
-    
-    //DOWNLOAD JSON- move to model
-    func downloadJSON()
-    {
+        let detailsVC: ImageDetailView = segue.destination as! ImageDetailView
         
-        let url = URL(string: urlString)
-        
-        var downloadTask = URLRequest(url: url!, cachePolicy: URLRequest.CachePolicy.reloadIgnoringCacheData, timeoutInterval: 5)
-        
-        downloadTask.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: downloadTask, completionHandler: {(data, response, error) -> Void in
-            
-            let jsonData: JSON? = JSON(data!)
-            if (jsonData != nil)
+        if segue.identifier == "open"
+        {
+            if let selectedRowIndexPath = tableView.indexPathForSelectedRow
             {
-                let photos = jsonData?["photos"].dictionaryValue
-                //print(photos!)
-                
-                for (key, element) in photos!
-                {
-                    if (key == "photo")
-                    {
-                        for item in element.arrayValue
-                        {
-                            let urlStr: String = {
-                                if let photoURL = item["url_z"].string {
-                                    return photoURL as! String
-                                }
-                                return "Not Found"
-                            }()
-                            
-                            let titleStr: String = {
-                                if let photoTitle = item["title"].string {
-                                    return photoTitle as! String
-                                }
-                                return "Untitled"
-                            }()
-                            
-                            let placeIDStr: String = {
-                                if let placeID = item["place_id"].string {
-                                    return placeID as! String
-                                }
-                                return "Not Found"
-                            }()
-                            
-                            let latStr: String = {
-                                if let photoLat = item["latitude"].string {
-                                    return photoLat as! String
-                                }
-                                return "Not Found"
-                            }()
-                            
-                            let longStr: String = {
-                                if let photoLong = item["longitude"].string {
-                                    return photoLong as! String
-                                }
-                                return "Not Found"
-                            }()
-                            
-                            let tagsStr: String = {
-                                if let photoTags = item["tags"].string {
-                                    return photoTags as! String
-                                }
-                                return "Not Found"
-                            }()
-                            
-                            //append to local array
-                            // photoTitle.append(item["title"].stringValue)
-                            // photoURL.append(item["url_z"].stringValue)
-                            
-                            //self.photoTags.append(item["tags"].stringValue)
-                            //self.photoLong.append(item["longtitude"].stringValue)
-                            //self.photoLat.append(item["latitude"].stringValue)
-                            
-                            
-                            print("__________________")
-                            print(item["title"])
-                            print(item["longitude"])
-                            print(item["latitude"])
-                            print(item["tags"])
-                            print(item["url_z"])
-                            //append
-                            
-                            
-                            self.photosArray.append(Photos(photoURL: urlStr, photoTitle: titleStr, placeID: placeIDStr, photoLat: latStr, photoLong: longStr, photoTags: tagsStr))
-                            
-                            
-                        }
-                    }
-                }
-                
-                OperationQueue.main.addOperation(
-                    {
-                        
-                        //update table here....!
-                        self.tableView.reloadData()
-                })
-                
+                let photo = Model.get.photosArray[selectedRowIndexPath.row]
+                //let photo = photosArray[selectedRowIndexPath.row]
+                detailsVC.photoURL = photo.photoURL
+                detailsVC.photoTitle = photo.photoTitle
+                detailsVC.photoLat = photo.photoLat
+                detailsVC.photoLong = photo.photoLong
             }
-            
-        }).resume()
-        
+        }
     }
+    
 
     
-}
     
-    //TO DO
-    //function to get 10 images to default feed
-    //function to get 10 more new images when scrolled (gesture)
+
+    
+}//end of class
+    
+
 
  
